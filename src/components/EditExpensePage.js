f4bd15f4bf8ceaeb54b5fc571f2bdcc6fceefd39
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-03-26 19:03:33 +0100

    redux done

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
-import { editExpense } from "../redux/actions/expenses";
+import { editExpense, removeExpense } from "../redux/actions/expenses";
 
 const EditExpensePage = props => {
 	console.log(props.expense);
 	return (
-		<div className="EditExpensePage">
-			<ExpenseForm
-				onSubmit={expense => {
-					props.dispatch(editExpense(props.expense.id, expense));
-					props.history.push("/");
-				}}
-				expense={props.expense}
-			/>
-		</div>
+		<div>
+		<ExpenseForm
+			expense={props.expense}
+			onSubmit={(expense) => {
+				props.dispatch(editExpense(props.expense.id, expense));
+				props.history.push('/');
+			}}
+		/>
+		<button onClick={() => {
+			props.dispatch(removeExpense({ id: props.expense.id }));
+			props.history.push('/');
+		}}>Remove</button>
+	</div>
 	);
 };
 
--- src/redux/store/configStore.js
+++ src/redux/store/configStore.js
@@ -1,16 +1,17 @@
-import {combineReducers, createStore} from "redux"
-import expensesReducer from "../reducers/expenses.reducer"
-import filtersReducer from "../reducers/filters.reducer"
+import { combineReducers, createStore } from "redux";
+import expensesReducer from "../reducers/expenses.reducer";
+import filtersReducer from "../reducers/filters.reducer";
 
 const configStore = () => {
-  const store = createStore(
-    combineReducers({
-      expenses: expensesReducer,
-      filters: filtersReducer,
-    })
-  );
+	const store = createStore(
+		combineReducers({
+			expenses: expensesReducer,
+			filters: filtersReducer,
+		}),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	);
 
-  return store
-}
+	return store;
+};
 
-export default configStore
+export default configStore;
